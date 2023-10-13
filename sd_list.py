cc4759b4864415104e63ecf6cac31d997310d155
servidor <servidor@Servidor>
2023-10-13 15:49:48 -0500

    final

--- sd_list.py
+++ sd_list.py
@@ -1,38 +1,51 @@
+from flask import json
 import queue
 import sys
 from threading import Lock
+import threading
 import sounddevice as sd
 
 from vosk import Model, KaldiRecognizer
 
 
-def Speaker_text( look :Lock ,cola :queue.Queue):
+class SPeaker :
     q = queue.Queue()
-    def callback(indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block."""
-        if status:
-            print(status, file=sys.stderr)
-        q.put(bytes(indata))
-    try:
-        device_info = sd.query_devices(9, "input")
-        print("!DEVICE INFO!")
-            # soundfile expects an int, sounddevice provides a float:
-        samplerate = int(device_info["default_samplerate"])
-        model = Model( r"/home/servidor/Documentos/Speach to text/vosk-model-small-es-0.42")
-        with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None,
-                    dtype="int16", channels=1, callback=callback):
-            
+    cola=queue.Queue()
+    lock = Lock()
+    def __init__(self) -> None:
+        t2 = threading.Thread(target=self._Speaker_text, args=[self.lock])
+        t2.start()
 
-                rec = KaldiRecognizer(model, samplerate)
-                print("*****REC******")
-                while True:
-                    data = q.get()
-                    if rec.AcceptWaveform(data):
-                        cola.put(rec.Result())
-                    else:
-                        cola.put(rec.PartialResult())
-    except KeyboardInterrupt:
-        print("\nDone")
-        exit(0)
+    def Get_data(self) -> str:
+        
+        return  str(self.cola.get())
+    def _Speaker_text(self, look :Lock ) ->None:
+        
+        def callback(indata, frames, time, status):
+            """This is called (from a separate thread) for each audio block."""
+            if status:
+                print(status, file=sys.stderr)
+            self.q.put(bytes(indata))
+        try:
+            device_info = sd.query_devices(0, "input")
+            print("!DEVICE INFO!")
+                # soundfile expects an int, sounddevice provides a float:
+            samplerate = int(device_info["default_samplerate"])
+            model = Model( r"/home/servidor/Documentos/Speach to text/vosk-model-small-es-0.42")
+            with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None,
+                        dtype="int16", channels=1, callback=callback):
+                
+
+                    rec = KaldiRecognizer(model, samplerate)
+                    print("*****REC******")
+                    while True:
+                        data = self.q.get()
+                        if rec.AcceptWaveform(data):
+                            self.cola.put(rec.Result())
+                        else:
+                            self.cola.put(rec.PartialResult())
+        except KeyboardInterrupt:
+            print("\nDone")
+            exit(0)
 
  
--- server.py
+++ server.py
@@ -2,26 +2,39 @@ from queue import Queue
 from threading import Lock
 import threading
 
-from flask import Flask
-from sd_list import  Speaker_text
+from flask import Flask,render_template
+from sd_list import  SPeaker
+import json
 Change_value=Queue()
 
+app = Flask(__name__)
 
-lock = Lock()
+
+ 
  
 
-app = Flask(__name__)
-t2 = threading.Thread(target=Speaker_text, args=[lock,Change_value])
-t2.start()
+
+"""
+SS=SPeaker()
+
+@app.route("/data")
+def Speaker():
+        return json.dump(json.loads(json.loads(SS.Get_data())))
+
+"""
+
+SS=SPeaker()
+
 @app.route("/")
-def hello_world():
-        b=str(Change_value.get())
-        print(b)
-        a="<h1>"+"Change_value"+""+b+"</h1>" 
-        return a
+def Index():
+       return render_template("index.html")
 
 
-
+@app.route("/data")
+def Speaker():
+        return SS.Get_data()
+        #return 
+        
 app.run(debug=False,port=5050,threaded=True)
 
  
